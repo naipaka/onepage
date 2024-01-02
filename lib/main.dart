cd9d325f57ebb5e7e75775ab78a739a364a1629b
naipaka <naipakapaka@gmail.com>
2024-01-02 20:26:25 +0900

    chore: Update analysis options and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:onepage/app.dart';
 
+import 'app.dart';
 import 'environment/environment.dart';
 
 Future<void> main() async {
