51b71d6b7d45c4e8a118279df8f28cdc9503d24a
naipaka <naipakapaka@gmail.com>
2024-11-24 13:54:59 +0900

    feat: Use adaptive CircularProgressIndicator for center loading indicator

--- packages/widgets/lib/src/loading/src/center_loading_indicator.dart
+++ packages/widgets/lib/src/loading/src/center_loading_indicator.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 /// A generic widget that displays a loading indicator in the center.
 const centerLoadingIndicator = Center(
-  child: CircularProgressIndicator(),
+  child: CircularProgressIndicator.adaptive(),
 );
