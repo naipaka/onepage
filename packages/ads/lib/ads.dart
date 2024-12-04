54d05f80ca0899e9fb8d9c317ebcffdd8f338f6b
naipaka <naipakapaka@gmail.com>
2024-12-04 09:29:32 +0900

    feat: Remove google_mobile_ads

--- packages/ads/lib/ads.dart
+++ packages/ads/lib/ads.dart
@@ -3,3 +3,4 @@ library;
 
 export 'src/ads_consent_client.dart';
 export 'src/app_tracking_transparency_client.dart';
+export 'src/initialize_ads.dart';
---
+++ packages/ads/lib/src/initialize_ads.dart
@@ -0,0 +1,6 @@
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+// /// Initializes ads.
+// Future<void> initializeAds() async {
+//   await MobileAds.instance.initialize();
+// }
