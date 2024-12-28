959236a5d95d9e415e750bbe2fcaa62992b78ce9
naipaka <naipakapaka@gmail.com>
2024-12-28 17:39:43 +0900

    chore: update Mockito version and regenerate mocks

--- packages/core/i18n/lib/src/gen/strings.g.dart
+++ packages/core/i18n/lib/src/gen/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 12 (6 per locale)
 ///
-/// Built on 2024-12-01 at 14:30 UTC
+/// Built on 2024-12-28 at 08:39 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
