2f82d6a1ab3b8b9a27474c196c298f26f63672ce
Ryota Kobayashi <45661924+naipaka@users.noreply.github.com>
2026-05-12 18:13:28 +0900

    chore: Flutter 3.41.9 & Dart 3.11.5 (#241)

--- packages/core/i18n/lib/src/gen/strings.g.dart
+++ packages/core/i18n/lib/src/gen/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 142 (71 per locale)
 ///
-/// Built on 2026-04-06 at 05:36 UTC
+/// Built on 2026-05-12 at 04:05 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
